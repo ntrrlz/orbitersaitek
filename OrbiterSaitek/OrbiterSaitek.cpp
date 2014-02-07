c77159ef8199728ec1f541ccdaf2927a20fc37fd
Wilco van den Berg <wilcovdberg@xs4all.nl>
2014-02-07 21:53:25 +0100

    release build and added signal strength indicator on standby display

--- OrbiterSaitek/OrbiterSaitek.cpp
+++ OrbiterSaitek/OrbiterSaitek.cpp
@@ -4,24 +4,31 @@
 
 #define ORBITER_MODULE 
 #include "orbitersdk.h" 
- 
+#include "OrbiterAPI.h"
 
 #include "hidapi.h"
 
+void increaseNav(VESSEL *vessel, DWORD navNum , bool decimal, bool increase );
+void parseNavData(VESSEL *vessel, DWORD navNum, unsigned char * buffer);
+
 HINSTANCE g_hDLL; 
- 
+
 hid_device *handle;
 
 DLLCLBK void InitModule (HINSTANCE hModule) 
 { 
-    g_hDLL = hModule; 
-    // perform global module initialisation here 
+	g_hDLL = hModule; 
+	// perform global module initialisation here 
 	handle = hid_open(0x06a3, 0x0d05, NULL);
+	oapiWriteLog("Device opened");
+	// Set the hid_read() function to be non-blocking.
+	hid_set_nonblocking(handle, 1);
+
 } 
 
 DLLCLBK void ExitModule (HINSTANCE hModule) 
 { 
-    // perform module cleanup here 
+	// perform module cleanup here 
 	hid_close(handle);
 	hid_exit();
 } 
@@ -30,6 +37,8 @@ DLLCLBK void ExitModule (HINSTANCE hModule)
 DLLCLBK void opcPreStep (double SimT, double SimDT, double mjd) // call every timestep
 {
 	unsigned char buf[23];
+	unsigned char inBuffer[5];
+
 
 	if (!handle)
 	{
@@ -37,65 +46,181 @@ DLLCLBK void opcPreStep (double SimT, double SimDT, double mjd) // call every ti
 	}
 
 	VESSEL *vessel = oapiGetFocusInterface(); // Get current vessel
-	
-	
+
+
 	if (vessel!=NULL) // check if pointer is valid
 	{
-		
-		DWORD navCount = vessel->GetNavCount();
-		DWORD nav1 = 0;
-		DWORD nav2 = 0;
-		if (navCount >=1) {
-			nav1 = vessel->GetNavChannel(0);
-			DWORD freq = nav1 * 5 + 10800;
-			buf[1]= (unsigned char) ((freq / 10000 ) % 10 );
-			buf[2]= (unsigned char) ((freq /1000 )% 10 );
-			buf[3]= (unsigned char) ((freq /100 ) % 10) + 0xd0 ; // add decimal point
-			buf[4]= (unsigned char) ((freq /10 ) % 10 );
-			buf[5]= (unsigned char) ((freq ) % 10 );
-		}
-		else 
-		{
-			buf[1] = 0x0f;
-			buf[2] = 0x0f;
-			buf[3] = 0x0f;
-			buf[4] = 0x0f;
-			buf[5] = 0x0f;
-		}
-		if (navCount >=2) {
-			nav2 = vessel->GetNavChannel(1);
-			DWORD freq = nav2 * 5 + 10800;
-			buf[11]= (unsigned char) ((freq / 10000 ) % 10 );
-			buf[12]= (unsigned char) ((freq /1000 )% 10 );
-			buf[13]= (unsigned char) ((freq /100 ) % 10) + 0xd0 ; // add decimal point
-			buf[14]= (unsigned char) ((freq /10 ) % 10 );
-			buf[15]= (unsigned char) ((freq ) % 10 );
-		}
-		else 
-		{
-			buf[11] = 0x0f;
-			buf[12] = 0x0f;
-			buf[13] = 0x0f;
-			buf[14] = 0x0f;
-			buf[15] = 0x0f;
-		}
 
-		buf[0] = 0x00;
-		buf[6] = 0x0f;
-		buf[7] = 0x0f;
-		buf[8] = 0x0f;
-		buf[9] = 0x0f;
-		buf[10] = 0x0f;
-		buf[16] = 0x0f;
-		buf[17] = 0x0f;
-		buf[18] = 0x0f;
-		buf[19] = 0x0f;
-		buf[20] = 0x0f;
-		buf[21] = 0x0f;
-		buf[22] = 0x00;
+		//0x01 >> 1 i u
+		//0x02 >> 1 i d
+		//0x04 >> 1 o u
+		//0x08 >> 1 o d
+
+		//0x10 >> 2 i u
+		//0x20 >> 2 i d
+		//0x40 >> 2 o u
+		//0x80 >> 2 o d
+		int bytesRead = hid_read(handle, inBuffer, sizeof(inBuffer));
+		if (bytesRead==3)
+		{
+			char keyByte = inBuffer[2];
+			if (keyByte & 0x01) 
+			{
+				increaseNav(vessel,0,true,true);
+			}
+			if (keyByte & 0x02)
+			{
+				increaseNav(vessel,0,true,false);
+			}
+			if (keyByte & 0x04) 
+			{
+				increaseNav(vessel,0,false,true);
+			}
+			if (keyByte & 0x08)
+			{
+				increaseNav(vessel,0,false,false);
+			}
+			if (keyByte & 0x10) 
+			{
+				increaseNav(vessel,1,true,true);
+			}
+			if (keyByte & 0x20)
+			{
+				increaseNav(vessel,1,true,false);
+			}
+			if (keyByte & 0x40) 
+			{
+				increaseNav(vessel,1,false,true);
+			}
+			if (keyByte & 0x80)
+			{
+				increaseNav(vessel,1,false,false);
+			}
+		}
+		buf[0]=0x00;
+		// parse nav1
+		parseNavData(vessel, 0, &(buf[1]));
+		// parse nav2
+		parseNavData(vessel, 1, &(buf[11]));
+
 		int res = hid_send_feature_report(handle, buf, 23);	
-
-		
 	}
+
+
+}
+
+void parseNavData(VESSEL *vessel, DWORD navNum, unsigned char * buffer)
+{
+	if (vessel->GetNavCount() > navNum) {
+		// vessel has nav
+		DWORD navValue = vessel->GetNavChannel(navNum);
+		DWORD freq = navValue * 5 + 10800;
+		buffer[0]= (unsigned char) ((freq / 10000 ) % 10 );
+		buffer[1]= (unsigned char) ((freq /1000 )% 10 );
+		buffer[2]= (unsigned char) ((freq /100 ) % 10) + 0xd0 ; // add decimal point
+		buffer[3]= (unsigned char) ((freq /10 ) % 10 );
+		buffer[4]= (unsigned char) ((freq ) % 10 );
+
+		NAVHANDLE navHandle = vessel->GetNavSource(navNum);
+		if (navHandle != NULL) 
+		{
+			// nav has signal
+			VECTOR3 gpos;
+			vessel->GetGlobalPos(gpos);
+
+			double signal = oapiGetNavSignal(navHandle, gpos);
+
+			buffer[5] = (signal > 1) ? 0xe0 : 0x0f;
+			buffer[6] = (signal > 10) ? 0xe0 : 0x0f;
+			buffer[7] = (signal > 100) ? 0xe0 : 0x0f;
+			buffer[8] = (signal > 1000) ? 0xe0 : 0x0f;
+			buffer[9] = (signal > 10000) ? 0xe0 : 0x0f;
+
+		}
+		else 
+		{
+			// no signal -> blank indicator
+			buffer[5] = 0x0f;
+			buffer[6] = 0x0f;
+			buffer[7] = 0x0f;
+			buffer[8] = 0x0f;
+			buffer[9] = 0x0f;
+		}
+
+	}
+	else 
+	{
+		// no nav radio -> all blank
+		buffer[0] = 0x0f;
+		buffer[1] = 0x0f;
+		buffer[2] = 0x0f;
+		buffer[3] = 0x0f;
+		buffer[4] = 0x0f;
+		buffer[5] = 0x0f;
+		buffer[6] = 0x0f;
+		buffer[7] = 0x0f;
+		buffer[8] = 0x0f;
+		buffer[9] = 0x0f;
+
+	}
+
+}
+
+void increaseNav(VESSEL *vessel, DWORD navNum , bool decimal, bool increase )
+{
+	DWORD navCount = vessel->GetNavCount();
+	if (navCount <= navNum)
+		return;
+
+	DWORD nav = vessel->GetNavChannel(navNum);
+	DWORD upper = nav / 20;
+	DWORD lower = nav % 20;
+	if (decimal && increase)
+	{
+		if (lower==19) 
+		{
+			lower = 0;
+		}
+		else 
+		{
+			lower++;
+		}
+	}
+	if (decimal && !increase)
+	{
+		if (lower==0) 
+		{
+			lower = 19;
+		}
+		else 
+		{
+			lower--;
+		}
+	}
+	if (!decimal && increase)
+	{
+		if (upper==31) 
+		{
+			upper = 0;
+		}
+		else 
+		{
+			upper++;
+		}
+	}
+	if (!decimal && !increase)
+	{
+		if (upper==0) 
+		{
+			upper = 31;
+		}
+		else 
+		{
+			upper--;
+		}
+	}
+	nav=upper * 20 + lower;
+	vessel->SetNavChannel(navNum,nav);
+
 }
 
